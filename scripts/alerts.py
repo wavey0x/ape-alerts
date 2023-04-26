9f3afc78152e3fd9c340db9c170aa3511ef44b61
wavey0x <retrowaveyy@gmail.com>
2023-04-26 11:58:12 -0400

    chore: disable seasolver alerts

--- scripts/alerts.py
+++ scripts/alerts.py
@@ -57,7 +57,7 @@ def main():
     alert_fee_distributor(last_block, current_block)
     alert_bribes(last_block, current_block)
     alert_ycrv(last_block, current_block)
-    alert_seasolver(last_block, current_block)
+    # alert_seasolver(last_block, current_block)
     # find_reverts(address_list, last_block, current_block)
 
     data['last_block'] = current_block
